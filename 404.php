9519fdc4a5cdb5f6b930a8cc6a676e621583d99f
yoan assan <e2376012@cmaisonneuve.qc.ca>
2025-03-24 15:41:32 -0400

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+    <h1>-------- 404---------</h1>
+    <section class="hero" style="background-image: url(<?= $hero_background ?>);"></section>
+    <?php get_footer(); ?>
+   
+</body>
+</html>
