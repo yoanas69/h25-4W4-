72083b6c9409487d17fec8309ec9b0f67d4297d3
yoan assan <e2376012@cmaisonneuve.qc.ca>
2025-03-24 15:55:34 -0400

    ajout de logo et search

--- 404.php
+++ 404.php
@@ -3,7 +3,12 @@
 
     <h1>-------- 404---------</h1>
     <section class="hero" style="background-image: url(<?= $hero_erreur?>);">
-        
+        <h1>page non trouvé</h1>
+        <figure class="entete__logo">
+                 <?php if (function_exists('the_custom_logo')) {
+                the_custom_logo();
+            } ?>
+        <?php get_search_form(); ?>
     </section>
 
     <?php get_footer(); ?>
